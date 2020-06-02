60ee0bfd96184d1afe4b61c809262333dafa7104
nickglovdev <nickglovdev@gmail.com>
2020-06-02 12:01:43 -0500

    I have just finished the dependency way of doing things for fish.

---
+++ scripts/fish/fishData.js
@@ -0,0 +1,56 @@
+const fishCollection = [
+    {
+        species: "Mr. Grumpy",
+        length: 3,
+        name: "Guppy",
+        location: "Ocean of Moody",
+        food: "Chips",
+        picture:"https://epond.eu/wp-content/uploads/2019/12/Purple-Queen-Guppy-Males.jpg"
+
+    },
+    {
+        species: "Parrot Fish",
+        length: 8,
+        name: "Angus",
+        location: "Sea of Google",
+        food: "algae",
+        picture: "https://i.ytimg.com/vi/y6tdGVTgmm4/maxresdefault.jpg"
+
+    },
+    {
+        species: "Puffer",
+        length: 4,
+        name: "Tim",
+        location: "Sea of Tools",
+        food: "Nails",
+        picture:"https://www.seattleaquarium.org/sites/default/files/images/shutterstock_83594650.jpg"
+
+    },
+    {
+        species: "King Fish",
+        length: 3,
+        name: "Lord Fishy Pants",
+        location: "Sea of Royalty",
+        food: "Gold",
+        picture:"https://www.nationalgeographic.com/content/dam/animals/pictures/hero/fish-hero.adapt.1900.1.jpg"
+
+    },
+    {
+        species: "Forhead Kissing Fish",
+        length: 5,
+        name: "Kissingly",
+        location: "Sea of Love",
+        food: "Love",
+        picture:"https://www.thisiscolossal.com/wp-content/uploads/2019/06/tsubaki-12.jpg"
+
+    },
+    {
+        species: "Flying Fish",
+        length: 6,
+        name: "Co-Pilot Troy",
+        location: "The Skys of Arcadia",
+        food: "Sky Pirates",
+        picture:"https://cdn.mos.cms.futurecdn.net/RY2EpSo74hvYXyAVpTN2Gg-1200-80.jpg"
+
+    }
+]
