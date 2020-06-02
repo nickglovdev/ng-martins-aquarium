1c1f1e7415a69e5007675db90df875a83adbdd52
nickglovdev <nickglovdev@gmail.com>
2020-06-02 13:59:46 -0500

    Added the quotes to be from the quoteList. Also made some other HTML changes.

---
+++ scripts/tip/tipData.js
@@ -0,0 +1,11 @@
+const quoteCollection = [
+    {
+        tip: "Forage brooklyn prism shaman readymade hammock selvage hot chicken. -That Guy",
+    },
+    {
+        tip: "Forage brooklyn prism shaman readymade hammock selvage hot chicken. -That Other Guy",
+    },
+    {
+        tip: "Forage brooklyn prism shaman readymade hammock selvage hot chicken. -The Hip Gal"
+    }
+]
