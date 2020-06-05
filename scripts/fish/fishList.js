29593d5f07431734246de074af52b473b4b3195e
nickglovdev <nickglovdev@gmail.com>
2020-06-05 10:54:49 -0500

    Just finished a select for fish

--- scripts/fish/fishList.js
+++ scripts/fish/fishList.js
@@ -1,3 +1,28 @@
+// Displays only holy fish from selection
+const fishTypeDropdown = document.querySelector(".typeChoice")
+const contentTarget = document.querySelector(".fishList")
+
+const clearFishList = () => contentTarget.innerHTML = ""
+
+
+fishTypeDropdown.addEventListener("change", (changeEvent) => {
+    // Get the value of the option chosen by the user
+    const userChoice = changeEvent.target.value
+
+    // If the user chose Holy, clear the list and only show holy fish
+    if (userChoice === "holy") {
+        clearFishList()
+        showHolyFish()
+    } else if (userChoice === "soldier") {
+        clearFishList()
+        showSoldierFish()
+    } else if (userChoice === "plebs") {
+        clearFishList()
+        showCommonFish()
+    }
+})
+
+
 // Function to show holy fish in the browser
 const showHolyFish = () => {
     const holyFish = mostHolyFish()
